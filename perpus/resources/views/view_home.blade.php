41014ae45e9d926e70fbec6beb58ca9780a4b355
diajengvesha <diajengvesharamadhini@gmail.com>
2024-06-23 19:06:40 +0700

    first commit

---
+++ perpus/resources/views/view_home.blade.php
@@ -0,0 +1,13 @@
+@extends('index')
+@section('title', 'home')
+
+@section('isihalaman')
+    <h3>Perpustakaan itu kayak harta karun, tiap buku adalah hidden gem</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+    Bayangin deh, perpustakaan itu kayak tempat penuh harta karun. Setiap rak buku di situ tuh kayak peti harta yang nyimpen sesuatu yang keren banget.
+    Buku-buku di sana itu bagaikan permata tersembunyi yang berharga tapi belum banyak orang yang tahu. Pas kamu buka buku,
+    rasanya kayak nemu permata yang penuh dengan pengetahuan, 
+    cerita seru, dan wawasan baru. Setiap buku bisa ngubah cara pandang kamu, kasih insight baru, atau bawa kamu ke dunia yang belum pernah kamu jelajahi.
+    Jadi, perpustakaan itu tempat yang tiap kali kamu kunjungin, selalu ada aja penemuan baru yang berharga, persis kayak nemuin harta karun.
+@endsection
